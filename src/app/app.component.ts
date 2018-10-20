2298b88e2876dba4d3178c773b481a65603fc1e6
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-10-20 11:54:23 +0530

    articles, and tags for global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from './article.service';
+import { TagsService } from './tags/tags.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'conduitApp';
+export class AppComponent implements OnInit {
+  title = 'conduict';
+
+  constructor(private articleService: ArticleService, private tagService: TagsService, private userSerive: UserService) {
+
+  }
+
+  ngOnInit(){
+    	if(localStorage.getItem('token')!=null){
+        this.userSerive.getCurrentUser().subscribe((user:{user: any}) => {
+          console.log(user.user.username);
+          this.userSerive.setUsername(user.user.username);
+        })
+      }
+  }
+
+  onTagSelection(value) {
+    console.log(value)
+    this.tagService.setTagName(value)
+    this.articleService.getFeedByTag(value)
+  }
 }
--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { TagsService } from './tags.service';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-tags',
@@ -6,10 +8,22 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent implements OnInit {
-
-  constructor() { }
+  tags : any
+  @Output() isTagSelected = new EventEmitter<string>();
+  constructor(private articleService: ArticleService , private tagService: TagsService) { }
 
   ngOnInit() {
+    this.tagService.getGlobalTags().subscribe((data : {tags:string}) => {
+      this.tags=data.tags
+    });
   }
 
+  tagSelected(tag){
+    this.isTagSelected.emit(tag)
+  }
+
+  onTagSelection(value) {
+    this.tagService.setTagName(value)
+    this.articleService.getFeedByTag(value)
+  }
 }
