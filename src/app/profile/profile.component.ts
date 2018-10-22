b8fa7021f276b539cc29afc9758824248155bbf8
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-10-22 12:42:36 +0530

    pagination added but with bootstrap errors

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../article.service';
 import { UserService } from '../user.service';
 import { ProfileService } from './profile.service';
-import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { ActivatedRoute, Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,15 +11,26 @@ import { ActivatedRoute } from '../../../node_modules/@angular/router';
 })
 export class ProfileComponent implements OnInit {
   username: string
+  image : string
+  isFollowing : boolean
+  loggedInUsername : string
   constructor(private articleService: ArticleService, private userService: UserService,
-    private profileService: ProfileService, private route: ActivatedRoute) { }
+    private profileService: ProfileService, private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.userService.currentUsername.subscribe((data : any) => {
+      this.loggedInUsername = data
+    })
+
     this.route.paramMap.subscribe(
       params => {
         this.username = params['params'].username
-        this.profileService.getUserProfile(this.username).subscribe((data) => {
-
+        this.profileService.getUserProfile(this.username).subscribe((data : any) => {
+          this.image = data.profile.image
+          this.isFollowing = data.profile.following
+        },error => {
+          this.router.navigate(['/']);
         })
         this.articleService.setMyArticles(this.username)
       })
